701a1aa1aa14c089938a584f8cf6422dbd5a5053
Fu-Qingchen <fuqingchen@whut.edu.cn>
2020-08-17 16:52:25 +0800

    完成第八章IO库的总结

--- Chapter8.TheIOLibrary/Practice8.1.cpp
+++ Chapter8.TheIOLibrary/Practice8.1.cpp
@@ -1,11 +1,17 @@
+#include<sstream>
 #include<iostream>
 using namespace std;
 int main(){
     istream & practice(istream &);
+
+    // Practice8.1
     practice(cin);
+    // Practice8.9
+    istringstream stream("Hello World");
+    practice(stream);
+
     return 0;
 }
-
 istream & practice(istream & input){
     char c;
     while(!input.eof()){
---
+++ Chapter8.TheIOLibrary/Practice8.10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
+int main(int argc, char *argv[]){
+    ifstream input(argv[1]);
+    string line, temp;
+    vector<string> lines;
+    while (getline(input, line))
+    {
+        istringstream iss(line);
+        while (iss >> temp)
+        {
+            lines.push_back(temp);
+        }
+    }
+    for (auto &i : lines)
+    {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
