3ef168324736ada4996cecba13082328992ea348
Fu_Qingchen <fuqingchen.gov@gmail.com>
2021-03-13 16:05:23 +0800

    完成：回溯算法和DFS

---
+++ QTProject/Calculator/calculator/release/moc_widget.cpp
@@ -0,0 +1,205 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../widget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_Widget_t {
+    QByteArrayData data[23];
+    char stringdata0[502];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Widget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Widget_t qt_meta_stringdata_Widget = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Widget"
+QT_MOC_LITERAL(1, 7, 22), // "on_pushButton0_clicked"
+QT_MOC_LITERAL(2, 30, 0), // ""
+QT_MOC_LITERAL(3, 31, 24), // "on_pushButtonDot_clicked"
+QT_MOC_LITERAL(4, 56, 22), // "on_pushButton1_clicked"
+QT_MOC_LITERAL(5, 79, 22), // "on_pushButton2_clicked"
+QT_MOC_LITERAL(6, 102, 22), // "on_pushButton3_clicked"
+QT_MOC_LITERAL(7, 125, 22), // "on_pushButton4_clicked"
+QT_MOC_LITERAL(8, 148, 22), // "on_pushButton5_clicked"
+QT_MOC_LITERAL(9, 171, 22), // "on_pushButton6_clicked"
+QT_MOC_LITERAL(10, 194, 22), // "on_pushButton7_clicked"
+QT_MOC_LITERAL(11, 217, 22), // "on_pushButton8_clicked"
+QT_MOC_LITERAL(12, 240, 22), // "on_pushButton9_clicked"
+QT_MOC_LITERAL(13, 263, 26), // "on_pushButtonEnter_clicked"
+QT_MOC_LITERAL(14, 290, 24), // "on_pushButtonDiv_clicked"
+QT_MOC_LITERAL(15, 315, 24), // "on_pushButtonMul_clicked"
+QT_MOC_LITERAL(16, 340, 24), // "on_pushButtonSub_clicked"
+QT_MOC_LITERAL(17, 365, 24), // "on_pushButtonAdd_clicked"
+QT_MOC_LITERAL(18, 390, 24), // "on_pushButtonLOP_clicked"
+QT_MOC_LITERAL(19, 415, 24), // "on_pushButtonROP_clicked"
+QT_MOC_LITERAL(20, 440, 26), // "on_pushButtonClear_clicked"
+QT_MOC_LITERAL(21, 467, 25), // "on_pushButtonBack_clicked"
+QT_MOC_LITERAL(22, 493, 8) // "timerOut"
+
+    },
+    "Widget\0on_pushButton0_clicked\0\0"
+    "on_pushButtonDot_clicked\0"
+    "on_pushButton1_clicked\0on_pushButton2_clicked\0"
+    "on_pushButton3_clicked\0on_pushButton4_clicked\0"
+    "on_pushButton5_clicked\0on_pushButton6_clicked\0"
+    "on_pushButton7_clicked\0on_pushButton8_clicked\0"
+    "on_pushButton9_clicked\0"
+    "on_pushButtonEnter_clicked\0"
+    "on_pushButtonDiv_clicked\0"
+    "on_pushButtonMul_clicked\0"
+    "on_pushButtonSub_clicked\0"
+    "on_pushButtonAdd_clicked\0"
+    "on_pushButtonLOP_clicked\0"
+    "on_pushButtonROP_clicked\0"
+    "on_pushButtonClear_clicked\0"
+    "on_pushButtonBack_clicked\0timerOut"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Widget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      21,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  119,    2, 0x08 /* Private */,
+       3,    0,  120,    2, 0x08 /* Private */,
+       4,    0,  121,    2, 0x08 /* Private */,
+       5,    0,  122,    2, 0x08 /* Private */,
+       6,    0,  123,    2, 0x08 /* Private */,
+       7,    0,  124,    2, 0x08 /* Private */,
+       8,    0,  125,    2, 0x08 /* Private */,
+       9,    0,  126,    2, 0x08 /* Private */,
+      10,    0,  127,    2, 0x08 /* Private */,
+      11,    0,  128,    2, 0x08 /* Private */,
+      12,    0,  129,    2, 0x08 /* Private */,
+      13,    0,  130,    2, 0x08 /* Private */,
+      14,    0,  131,    2, 0x08 /* Private */,
+      15,    0,  132,    2, 0x08 /* Private */,
+      16,    0,  133,    2, 0x08 /* Private */,
+      17,    0,  134,    2, 0x08 /* Private */,
+      18,    0,  135,    2, 0x08 /* Private */,
+      19,    0,  136,    2, 0x08 /* Private */,
+      20,    0,  137,    2, 0x08 /* Private */,
+      21,    0,  138,    2, 0x08 /* Private */,
+      22,    1,  139,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+
+       0        // eod
+};
+
+void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Widget *_t = static_cast<Widget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton0_clicked(); break;
+        case 1: _t->on_pushButtonDot_clicked(); break;
+        case 2: _t->on_pushButton1_clicked(); break;
+        case 3: _t->on_pushButton2_clicked(); break;
+        case 4: _t->on_pushButton3_clicked(); break;
+        case 5: _t->on_pushButton4_clicked(); break;
+        case 6: _t->on_pushButton5_clicked(); break;
+        case 7: _t->on_pushButton6_clicked(); break;
+        case 8: _t->on_pushButton7_clicked(); break;
+        case 9: _t->on_pushButton8_clicked(); break;
+        case 10: _t->on_pushButton9_clicked(); break;
+        case 11: _t->on_pushButtonEnter_clicked(); break;
+        case 12: _t->on_pushButtonDiv_clicked(); break;
+        case 13: _t->on_pushButtonMul_clicked(); break;
+        case 14: _t->on_pushButtonSub_clicked(); break;
+        case 15: _t->on_pushButtonAdd_clicked(); break;
+        case 16: _t->on_pushButtonLOP_clicked(); break;
+        case 17: _t->on_pushButtonROP_clicked(); break;
+        case 18: _t->on_pushButtonClear_clicked(); break;
+        case 19: _t->on_pushButtonBack_clicked(); break;
+        case 20: _t->timerOut((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject Widget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_Widget.data,
+      qt_meta_data_Widget,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *Widget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Widget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_Widget.stringdata0))
+        return static_cast<void*>(const_cast< Widget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 21)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 21;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 21)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 21;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
